756a9977de961323ff251f0a353c3b5eeb0a9a7c
Angell Pimienta's <angelefrain1510@gmail.com>
2025-03-08 15:51:42 -0500

    fix: general package

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
+import { ProvidersModule } from './providers/providers.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/moviesDB'), MoviesModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/proveedorDB'),
+    MongooseModule.forRoot('mongodb://localhost:27017/moviesDB'),
+    MoviesModule,
+    ProvidersModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
